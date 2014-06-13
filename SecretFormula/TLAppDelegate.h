eee071fcbae7c733900090cdf64ddf5c8c7393a0
Erik Engheim <erik.engheim@mac.com>
2014-06-13 23:36:46 +0200

    Initial Commit

---
+++ SecretFormula/TLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TLAppDelegate.h
+//  SecretFormula
+//
+//  Created by Erik Engheim on 6/13/14.
+//  Copyright (c) 2014 Erik Engheim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SecretFormula/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SecretFormula
+//
+//  Created by Erik Engheim on 6/13/14.
+//  Copyright (c) 2014 Erik Engheim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TLAppDelegate class]));
+    }
+}
